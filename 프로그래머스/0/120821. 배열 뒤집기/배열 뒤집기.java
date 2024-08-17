34f2e3b7b6902f14e780be7f99ea889f82ed579c
dltjdnjs <148112372+SeoWonLeee@users.noreply.github.com>
2024-08-18 03:32:34 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length];
-        int index = 0;
         
-        for(int i=num_list.length-1; i>=0; i--) {
-            answer[index] = num_list[i];
-            index++;
+        for(int i=0; i<num_list.length; i++) {
+            answer[i] = num_list[num_list.length-i-1];
         }
         
         return answer;
