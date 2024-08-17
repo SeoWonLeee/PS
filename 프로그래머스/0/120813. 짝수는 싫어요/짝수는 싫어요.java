f613d8a0952689cfc36916e6626e6af677cf94ba
dltjdnjs <148112372+SeoWonLeee@users.noreply.github.com>
2024-08-18 02:28:03 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.08 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=1; i<=n; i++) {
+            if(i%2 != 0) {
+                list.add(i);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int j=0; j<list.size(); j++) {
+            answer[j] = list.get(j);
+        }
+        
+        return answer;
+    }
+}
