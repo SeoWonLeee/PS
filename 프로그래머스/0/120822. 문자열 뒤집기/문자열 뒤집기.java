1eb92604830c0ce0a83686d9c67cb103239f25f7
dltjdnjs <148112372+SeoWonLeee@users.noreply.github.com>
2024-08-18 15:30:16 +0900

    [level 0] Title: 문자열 뒤집기, Time: 1.46 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        String arr[] = my_string.split("");
+        
+        for(int i=0; i<my_string.length(); i++) {
+            answer += arr[arr.length-i-1];
+        }
+        
+        return answer;
+    }
+}
